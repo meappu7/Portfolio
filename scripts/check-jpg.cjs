9b6d94619e91f9eb933e0b089fc2ba602ef1d115
meappu7 <meappu7@users.noreply.github.com>
2026-09-17 18:53:44 +0530

    chore: remove helper script

--- scripts/check-jpg.cjs
+++
@@ -1,6 +0,0 @@
-﻿const fs = require("fs");
-const path = require("path");
-
-const file = path.join(__dirname, "../public/assets/media/intro.jpg");
-const content = fs.readFileSync(file, "utf8");
-console.log("Start of intro.jpg:", content.slice(0, 60));
